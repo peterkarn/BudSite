cb9581f6823e492775a5868abd923f0bb45fda81
karnauhmax <91369724+karnauhmax@users.noreply.github.com>
2022-04-25 19:43:47 +0300

    Add files via upload

---
+++ src/js/_functions.js
@@ -0,0 +1,5 @@
+import { mobileCheck } from "./functions/mobile-check";
+console.log(mobileCheck());
+
+import { DynamicAdapt } from "./functions/dynamicAdapt";
+console.log(DynamicAdapt);
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import "./_vendor";
+import vars from "./_vars";
+import "./_functions";
+import "./_components";
+// import Dropdown from "../../node_modules/bootstrap/dist/js/bootstrap.min";
+import Dropdown from "../../node_modules/bootstrap/js/dist/dropdown";
+import Carousel from "../../node_modules/bootstrap/js/dist/carousel";
