47cb909c876efd678970a869aacd1289da26d7fa
Peter Karnaukh <petr.karnaukh@together.com>
2022-04-27 07:20:12 +0300

    added faq and info section

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,6 @@ import vars from "./_vars";
 import "./_functions";
 import "./_components";
 import Dropdown from "../../node_modules/bootstrap/dist/js/bootstrap.min";
+import Collapse from "../../node_modules/bootstrap/js/dist/collapse";
 // import Dropdown from "../../node_modules/bootstrap/js/dist/dropdown";
 // import Carousel from "../../node_modules/bootstrap/js/dist/carousel";
