0d47ad53705edb64556f11304bf7efda1bab471d
Max Karnauh <krnhmx@gmail.com>
2022-05-01 14:47:14 +0300

    finished footer started doing modal

--- src/js/main.js
+++ src/js/main.js
@@ -4,24 +4,13 @@ import "./_functions";
 import "./_components";
 import Dropdown from "../../node_modules/bootstrap/dist/js/bootstrap.min";
 import { hide } from "@popperjs/core";
+import { Splide } from "@splidejs/splide";
 
 let inputs = document.querySelectorAll("input[type='tel']");
 let im = new Inputmask("+380 (99) 999 99 99");
 
 im.mask(inputs);
 import Collapse from "../../node_modules/bootstrap/js/dist/collapse";
-// import Dropdown from "../../node_modules/bootstrap/js/dist/dropdown";
-// import Carousel from "../../node_modules/bootstrap/js/dist/carousel";
-
-// const filterBtns = document.querySelector(".filter__btns");
-// const filterItem = document.querySelectorAll(".filter__item");
-
-// document.querySelector(".filter__btns").addEventListener("click", (event) => {
-//   if (event.target.tagName !== "BUTTON") return false;
-
-//   let filterClass = event.target.dataset("filter");
-//   console.log(filterClass);
-// });
 
 const filterItem = document.querySelectorAll(".filter__item");
 const filterBtns = document.querySelector(".filter__btns");
@@ -33,8 +22,6 @@ filterCategories.addEventListener("click", (event) => {
   }
 
   let filterCategory = event.target.dataset.category;
-  console.log(filterCategory);
-
   filterItem.forEach((element) => {
     if (!element.classList.contains(filterCategory)) {
       element.classList.add("hide");
@@ -95,8 +82,6 @@ tabsParent.forEach((parent) => {
     }
   });
 });
-
-console.log(tabsParent);
 
 // splide
 
